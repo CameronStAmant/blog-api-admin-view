9816714f5aa22e66b2df5e6ca4dfe7e5bb2a3a5d
CameronStAmant <40364114+CameronStAmant@users.noreply.github.com>
2021-05-01 06:12:27 -0600

    Add toggle button for publish state

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,6 +5,20 @@ import Layout from './Layout';
 const Home = () => {
   const [posts, setPosts] = useState(null);
 
+  const handleSubmit = (post) => {
+    const requestOptions = {
+      method: 'PUT',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        title: post.title,
+        body: post.body,
+        published: !post.published,
+      }),
+    };
+    console.log(post);
+    fetch('http://localhost:3000/posts/' + post.id, requestOptions);
+  };
+
   useEffect(() => {
     const fetchPosts = async () => {
       const response = await fetch('http://localhost:3000/posts', {
@@ -13,11 +27,15 @@ const Home = () => {
       const data = await response.json();
       const item = data.posts;
       const listPosts = item.map((post) => {
-        console.log(post);
         return (
           <li key={post.title} className="postDetails">
             <a href={post.url}>{post.title}</a>
-            <p>Published: {post.published.toString()}</p>
+            <div>
+              <p>Published: {post.published.toString()}</p>
+              <form onSubmit={() => handleSubmit(post)}>
+                <input type="submit" value="Toggle" />
+              </form>
+            </div>
           </li>
         );
       });
