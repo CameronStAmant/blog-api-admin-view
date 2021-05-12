c55063a4a3902cd58fd6dfce6d4c6243b27bac1a
CameronStAmant <40364114+CameronStAmant@users.noreply.github.com>
2021-05-12 14:41:10 -0600

    Add comment reload on deletion

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -4,72 +4,74 @@ const { DateTime } = require('luxon');
 
 const Comments = (props) => {
   const [comments, setComments] = useState([]);
+  const [reloadComments, setReloadComments] = useState(false);
+
+  const handleDelete = (e, commentId) => {
+    e.preventDefault();
+    const requestOptions = {
+      method: 'DELETE',
+      mode: 'cors',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer ' + localStorage.getItem('user'),
+      },
+    };
+    fetch(
+      'https://serene-waters-04286.herokuapp.com/posts/' +
+        props.postid +
+        '/comments/' +
+        commentId,
+      requestOptions
+    ).then(() => {
+      setComments([]);
+      setReloadComments(!reloadComments);
+    });
+  };
+
+  const fetchComments = async () => {
+    const response = await fetch(
+      'https://serene-waters-04286.herokuapp.com/posts/' +
+        props.postid +
+        '/comments',
+      {
+        mode: 'cors',
+      }
+    );
+    const data = await response.json();
+    for (const comment of data.comments) {
+      let element;
+      element = (
+        <div className="commentLayout" id={comment._id} key={comment._id}>
+          <h4 className="commentAuthor">{comment.author.username}</h4>
+          <p className="commentTimestamp">
+            {DateTime.fromISO(comment.timestamp).toLocaleString(
+              DateTime.DATETIME_MED
+            )}
+          </p>
+          <p className="commentBody">{comment.body}</p>
+          <div className="modifyComment">
+            <form
+              action={
+                '/posts/' + props.postid + '/comments/' + comment._id + '/edit'
+              }
+            >
+              <input type="submit" value="Edit" />
+            </form>
+            <form onSubmit={(e) => handleDelete(e, comment._id)}>
+              <input type="submit" value="Delete" />
+            </form>
+          </div>
+
+          <br />
+        </div>
+      );
+      setComments((comments) => [...comments, element]);
+    }
+  };
 
   useEffect(() => {
-    const handleDelete = (commentId) => {
-      const requestOptions = {
-        method: 'DELETE',
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: 'Bearer ' + localStorage.getItem('user'),
-        },
-      };
-      fetch(
-        'https://serene-waters-04286.herokuapp.com/posts/' +
-          props.postid +
-          '/comments/' +
-          commentId,
-        requestOptions
-      );
-    };
-
-    const fetchComments = async () => {
-      const response = await fetch(
-        'https://serene-waters-04286.herokuapp.com/posts/' +
-          props.postid +
-          '/comments',
-        {
-          mode: 'cors',
-        }
-      );
-      const data = await response.json();
-
-      for (const comment of data.comments) {
-        let element;
-        element = (
-          <div className="commentLayout" id={comment._id} key={comment._id}>
-            <h4 className="commentAuthor">{comment.author.username}</h4>
-            <p className="commentTimestamp">
-              {DateTime.fromISO(comment.timestamp).toLocaleString(
-                DateTime.DATETIME_MED
-              )}
-            </p>
-            <p className="commentBody">{comment.body}</p>
-            <div className="modifyComment">
-              <form
-                action={
-                  '/posts/' +
-                  props.postid +
-                  '/comments/' +
-                  comment._id +
-                  '/edit'
-                }
-              >
-                <input type="submit" value="Edit" />
-              </form>
-              <form onSubmit={() => handleDelete(comment._id)}>
-                <input type="submit" value="Delete" />
-              </form>
-            </div>
-
-            <br />
-          </div>
-        );
-        setComments((comments) => [...comments, element]);
-      }
-    };
     fetchComments();
-  }, [props.postid]);
+  }, [reloadComments]);
   return <div className="Comment">{comments}</div>;
 };
 
