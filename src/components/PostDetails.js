95935ea5fb0389a75d59c618fd4eb7cae743746f
CameronStAmant <40364114+CameronStAmant@users.noreply.github.com>
2021-06-22 09:05:11 -0600

    Update PostDetails to use TailwindCSS

--- src/components/PostDetails.js
+++ src/components/PostDetails.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useParams, useLocation, useHistory, Link } from 'react-router-dom';
 
-import './PostDetails.css';
+// import './PostDetails.css';
 import Layout from './Layout';
 import Comments from './Comments';
 
@@ -48,18 +48,31 @@ const PostDetails = (props) => {
 
   return (
     <Layout authState={props.authState}>
-      <div className="postLayout">
-        <div className="post">
-          <h2>{postDetails ? postDetails.title : ''}</h2>
-          <br />
-          <Link to={useLocation().pathname + '/edit'}>
-            <button type="button">Edit</button>
-          </Link>
-          <button type="button" onClick={deletePost}>
-            Delete
-          </button>
-          <br />
-          <p> {postDetails ? postDetails.body : ''}</p>
+      <div className="display: grid grid-rows-post auto-rows-auto row-start-2 col-span-full">
+        <div className="display: grid grid-rows-postTitle">
+          <div className="row-start-1 place-self-center col-span-full text-2xl font-semibold text-center display: grid">
+            <h2>{postDetails ? postDetails.title : ''}</h2>
+            <div>
+              <div className="space-x-3">
+                <Link to={useLocation().pathname + '/edit'}>
+                  <button
+                    type="button"
+                    className="border-solid border-4 border-green-200 rounded-md bg-green-200 hover:bg-green-300 hover:border-green-300 w-auto active:bg-green-400 active:border-green-400 shadow-sm"
+                  >
+                    Edit
+                  </button>
+                </Link>
+                <button
+                  type="button"
+                  className="border-solid border-4 border-green-200 rounded-md bg-green-200 hover:bg-green-300 hover:border-green-300 w-auto active:bg-green-400 active:border-green-400 shadow-sm"
+                  onClick={deletePost}
+                >
+                  Delete
+                </button>
+              </div>
+            </div>
+          </div>
+          <p className="mx-2 px-1"> {postDetails ? postDetails.body : ''}</p>
         </div>
         <Comments postid={id} />
       </div>
