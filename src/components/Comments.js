ab4098d7cbab26b8e5c874fb738078e279284d36
CameronStAmant <40364114+CameronStAmant@users.noreply.github.com>
2021-06-22 09:03:49 -0600

    Update Comments to use TailwindCSS

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import './Comments.css';
 const { DateTime } = require('luxon');
 
 const Comments = (props) => {
@@ -42,29 +41,40 @@ const Comments = (props) => {
     for (const comment of data.comments) {
       let element;
       element = (
-        <div className="commentLayout" id={comment._id} key={comment._id}>
-          <h4 className="commentAuthor">{comment.author.username}</h4>
-          <p className="commentTimestamp">
+        <div
+          className="box-border border-solid m-2 shadow-md p-1 display: grid gap-2 grid-rows-2 grid-cols-2 auto-rows-min-content justify-self-stretch border-2 border-green-200 rounded-md "
+          id={comment._id}
+          key={comment._id}
+        >
+          <h4 className="justify-self-start">{comment.author.username}</h4>
+          <p className="justify-self-end">
             {DateTime.fromISO(comment.timestamp).toLocaleString(
               DateTime.DATETIME_MED
             )}
           </p>
-          <p className="commentBody">{comment.body}</p>
-          <div className="modifyComment">
+          <p className="row-start-2 col-span-full">{comment.body}</p>
+          <div className="justify-self-end space-x-3 col-start-2">
             <Link
               to={
                 '/posts/' + props.postid + '/comments/' + comment._id + '/edit'
               }
             >
-              <button type="button">Edit</button>
+              <button
+                type="button"
+                className="border-solid border-4 border-green-200 rounded-md bg-green-200 hover:bg-green-300 hover:border-green-300 w-auto active:bg-green-400 active:border-green-400 shadow-sm"
+              >
+                Edit
+              </button>
             </Link>
 
-            <button type="button" onClick={(e) => handleDelete(e, comment._id)}>
+            <button
+              type="button"
+              className="border-solid border-4 border-green-200 rounded-md bg-green-200 hover:bg-green-300 hover:border-green-300 w-auto active:bg-green-400 active:border-green-400 shadow-sm"
+              onClick={(e) => handleDelete(e, comment._id)}
+            >
               Delete
             </button>
           </div>
-
-          <br />
         </div>
       );
       setComments((comments) => [...comments, element]);
@@ -74,7 +84,7 @@ const Comments = (props) => {
   useEffect(() => {
     fetchComments();
   }, [reloadComments]);
-  return <div className="Comment">{comments}</div>;
+  return <div className="justify-self-stretch my-14">{comments}</div>;
 };
 
 export default Comments;
