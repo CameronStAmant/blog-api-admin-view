80006d69aebdba78229186d21feebe1ff75d395a
CameronStAmant <40364114+CameronStAmant@users.noreply.github.com>
2021-09-27 13:03:57 -0600

    Add margin on edit page

--- src/components/CommentEditPage.js
+++ src/components/CommentEditPage.js
@@ -4,13 +4,15 @@ import CommentForm from './CommentForm';
 const CommentEditPage = (props) => {
   return (
     <Layout authState={props.authState}>
-      <CommentForm
-        authState={props.authState}
-        userId={props.userId}
-        loadComments={() => props.setLoadComments(!props.loadComments)}
-        setComments={() => props.setComments([])}
-        comments={props.comments}
-      />
+      <div className="mx-4">
+        <CommentForm
+          authState={props.authState}
+          userId={props.userId}
+          loadComments={() => props.setLoadComments(!props.loadComments)}
+          setComments={() => props.setComments([])}
+          comments={props.comments}
+        />
+      </div>
     </Layout>
   );
 };
